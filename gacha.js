9345cdc5a8dfda8b227438b1c01b1351490ea814
tetsuyashibata0312 <shibata@chatplus.jp>
2026-01-02 10:03:44 +0900

    Create gacha.js

---
+++ gacha.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('gachaBtn');
+const result = document.getElementById('result');
+
+btn.addEventListener('click', () => {
+  result.textContent = 'ガチャ回したばい！😆';
+});
